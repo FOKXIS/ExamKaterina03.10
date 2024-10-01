ee302cf84d68e9913f05b40206e860eff56457f3
Vladimir <79203161+Primus-max@users.noreply.github.com>
2024-10-01 23:13:43 +0300

    Создали модальное окно,логику "открыть-закрыть"

--- scripts/Scripts.js
+++ scripts/Scripts.js
@@ -1,15 +1,26 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-      const showModal =  () => {
-        const formRegestrationModal=document.getElementById('FormRegestrationModal')
-        formRegestrationModal.style.display = 'block'; 
+document.addEventListener('DOMContentLoaded', () => {
+    const formRegestrationModal = document.getElementById('FormRegestrationModal');
+    const clouseButtonFormRegistrationModal = document.getElementById('clouseButtonFormRegistrationModal');
+    let isModalShow = false;
+    // функиця отображения модалки
+    const toggleModal = () => {
+        // переключалка 
+        if (isModalShow) {
+            formRegestrationModal.style.display = 'none';
+            isModalShow = false;
+        } else {
+            formRegestrationModal.style.display = 'block';
+            isModalShow = !isModalShow;
+        }
     };
     const FormRegestration = document.getElementById('FormRegestration')
-    FormRegestration.addEventListener('click',showModal)
+    FormRegestration.addEventListener('click', toggleModal);
+    clouseButtonFormRegistrationModal.addEventListener('click', toggleModal)
 
-  
+
 
 
 
 })
- 
+
 
